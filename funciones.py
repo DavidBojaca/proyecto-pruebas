58b7d8aa72c62f93d758b8d1f6c09ad923c4f529
Ubuntu <ubuntu@ip-172-31-77-216.ec2.internal>
2023-02-23 20:09:23 +0000

    funcion es primo

--- funciones.py
+++ funciones.py
@@ -5,6 +5,8 @@ Funtion module
 def sumar(x,y):
     return x+y
     
+#funcion para saber si un numero es primo
+    
 def es_primo(numero):
     return numero > 1 and all(numero % i != 0 for i in range(2, int(numero ** 0.5) + 1))
-   
+ 
